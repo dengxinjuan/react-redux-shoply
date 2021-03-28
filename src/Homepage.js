ac7bbc67452c3933ae08e0021df293d36f4ae7ad
xinjuan deng <dengxinj@gmail.com>
2021-03-27 21:01:35 -0400

    add some homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,15 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const image =
+    "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvcHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+  return (
+    <div className="display-4">
+      <div>
+        heeeellooooo!
+        <img src={image} alt="shop image" />
+      </div>
+      Yes! We are open!
+    </div>
+  );
 };
 
 export default Homepage;
--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -14,9 +14,11 @@ const ProductDetail = () => {
       <img src={products[id].image_url} alt="product" width="300px" />
       <p>{products[id].description}</p>
       <h2>Price is ${products[id].price}</h2>
-      <button onClick={() => dispath(addToCart(id))}>Add!</button>
+      <button className="bg-success" onClick={() => dispath(addToCart(id))}>
+        Add!
+      </button>
       <button onClick={() => dispath(removeFromCart(id))}>Remove</button>
-      <button className="btn-lg bg-danger" onClick={() => dispath(reset())}>
+      <button className=" bg-danger" onClick={() => dispath(reset())}>
         RESET
       </button>
     </div>
